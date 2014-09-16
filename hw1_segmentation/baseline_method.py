a3371ec185225e5d1f501d4fcdac8b37c4a02adc
Hexiang Hu <hexiangh@sfu.ca>
2014-09-16 15:20:47 -0700

    Init baseline method script file
    1) Add option parser
    2) Import basic package

---
+++ hw1_segmentation/baseline_method.py
@@ -0,0 +1,8 @@
+
+import sys, codecs, optparse, os
+
+optparser = optparse.OptionParser()
+optparser.add_option("-c", "--unigramcounts", dest='counts1w', default=os.path.join('data', 'count_1w.txt'), help="unigram counts")
+optparser.add_option("-b", "--bigramcounts", dest='counts2w', default=os.path.join('data', 'count_2w.txt'), help="bigram counts")
+optparser.add_option("-i", "--inputfile", dest="input", default=os.path.join('data', 'input'), help="input file to segment")
+(opts, _) = optparser.parse_args()
