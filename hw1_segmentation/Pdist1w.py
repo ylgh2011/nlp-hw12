db903774ec16cf4e6c9a13acb0adde974be4d693
kl <kaiyuanl@gmail.com>
2014-09-18 05:12:38 +0800

    Add the mark dictionary. Score now 84.46(78.12 on server)
    1) Add and use mark dictionary
    2) For all the chars that are not in the dict, connect them
    3) comment out the bygram for the score get bad with it

--- hw1_segmentation/Pdist1w.py
+++ hw1_segmentation/Pdist1w.py
@@ -2,7 +2,7 @@
 class Pdist1w(dict):
     "A probability distribution estimated from counts in datafile."
 
-    def __init__(self, filename, sep='\t', N=None, missingfn=None):
+    def __init__(self, filename, sep='\t', N=None):
         self.maxlen = 0 
         for line in file(filename):
             (key, freq) = line.split(sep)
@@ -13,10 +13,8 @@ class Pdist1w(dict):
             self[utf8key] = self.get(utf8key, 0) + int(freq)
             self.maxlen = max(len(utf8key), self.maxlen)
         self.N = float(N or sum(self.itervalues()))
-        self.missingfn = missingfn or (lambda k, N: 1./N)
 
     def __call__(self, key):
         if key in self: return float(self[key])/float(self.N)
         #else: return self.missingfn(key, self.N)
-        elif len(key) == 1: return self.missingfn(key, self.N)
         else: return None
