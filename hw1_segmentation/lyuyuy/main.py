a60a6882431c84c878a2ab78e20560f12f97bc71
Louis Ye <lvyuy@sfu.ca>
2014-09-18 21:02:23 -0700

    Reconstruct the code according to kl's code
    1) modify the maxlen of pw1 and pw2

--- hw1_segmentation/lyuyuy/main.py
+++ hw1_segmentation/lyuyuy/main.py
@@ -1,53 +1,116 @@
+#The is the main program to run
 import sys, codecs, optparse, os
-from heapq import heappush
-from heapq import heappop
 
-from TryLib import Pdist
-from TryLib import Entry
-from TryLib import updateHeap
-from TryLib import heapString
+optparser = optparse.OptionParser()
+optparser.add_option("-c", "--unigramcounts", dest='counts1w', default=os.path.join('data', 'count_1w.txt'), help="unigram counts")
+optparser.add_option("-b", "--bigramcounts", dest='counts2w', default=os.path.join('data', 'count_2w.txt'), help="bigram counts")
+optparser.add_option("-m", "--markcounts", dest='countsmark', default=os.path.join('data', 'count_mark.txt'), help="mark_counts")
+optparser.add_option("-i", "--inputfile", dest="input", default=os.path.join('data', 'input'), help="input file to segment")
+(opts, _) = optparser.parse_args()
 
-def main():
-    optparser = optparse.OptionParser()
-    optparser.add_option("-c", "--unigramcounts", dest='counts1w', default=os.path.join('data', 'count_1w.txt'), help="unigram counts")
-    optparser.add_option("-b", "--bigramcounts", dest='counts2w', default=os.path.join('data', 'count_2w.txt'), help="bigram counts")
-    optparser.add_option("-i", "--inputfile", dest="input", default=os.path.join('data', 'input'), help="input file to segment")
-    (opts, _) = optparser.parse_args()
+from Entry import Entry
+from Pdist1w import Pdist1w
+from Pdist2w import Pdist2w
+from math import log
 
-    Pw = Pdist(opts.counts1w)
-    input_str = unicode(file(opts.input).read(), 'utf-8')
+old = sys.stdout
+sys.stdout = codecs.lookup('utf-8')[-1](sys.stdout)
 
-    heap = []
-    chart = {}
-    updateHeap(heap, 0, input_str, Pw, 0, None)
+P1w = Pdist1w(opts.counts1w, singleCharIgnore = 7)
+P2w = Pdist2w(opts.counts2w)
+Pm = Pdist1w(opts.countsmark)
+sumN = P1w.N + P2w.N + Pm.N
+P1w.N = P2w.N = Pm.N = sumN
 
-    while heap:
-        curEntry = heappop(heap)
-        endIndex = curEntry.start_pos + len(curEntry.word) - 1
-        if endIndex in chart:
-            preEntry = chart[endIndex]
-            if preEntry.log_prob < curEntry.log_prob:
-                chart[endIndex] = curEntry
+with open(opts.input) as f:
+    for line in f:
+        utf8line = unicode(line.strip(), 'utf-8')
+        entryList = []
+        for index in range(len(utf8line)):
+            maxSum = float('-Inf')
+            maxEnt = 0
+            maxWord = None
+            inDict = False
+#Do the bygram match
+            """
+            headEntry = index - P1w.maxlen
+            if (headEntry < 0):
+                headEntry = 0
+            for i in range(headEntry, index + 1):
+                if (i == 0):
+                    preLogP = 0
+                    isHead = True
+                else:
+                    preLogP = entryList[i - 1].logP
+                    isHead = False
+                thisP = P2w(utf8line[i:index+1], isHead)
+                if (thisP is None):
+                    thisLogP = float('-Inf')
+                else:
+                    thisLogP = log(thisP["p"])
+                if (thisLogP + preLogP > maxSum):
+                    maxSum = thisLogP + preLogP
+                    maxEnt = i - 1
+                    maxWord = thisP["word"]
+                    inDict = True
+"""
+#Do the unigram match
+            if (inDict is False):
+                headEntry = index - max(P1w.maxlen, P2w.maxlen)
+                if (headEntry < 0):
+                    headEntry = 0
+                for i in range(headEntry, index + 1):
+                    if (i == 0):
+                        preLogP = 0
+                    else:
+                        preLogP = entryList[i - 1].logP
+                    thisP = P1w(utf8line[i:index+1])
+                    if (thisP is None):
+                        thisLogP = float('-Inf')
+                    else:
+                        thisLogP = log(thisP)
+                    if (thisLogP + preLogP > maxSum):
+                        maxSum = thisLogP + preLogP
+                        maxEnt = i - 1
+                        maxWord = utf8line[maxEnt + 1:index + 1] 
+                        inDict = True
+
+#Do the markgram match
+            if (inDict is False):
+                headEntry = index - Pm.maxlen
+                if (headEntry < 0):
+                    headEntry = 0
+                for i in range(headEntry, index + 1):
+                    if (i == 0):
+                        preLogP = 0
+                    else:
+                        preLogP = entryList[i - 1].logP
+                    thisP = Pm(utf8line[i:index+1])
+                    if (thisP is None):
+                        thisLogP = float('-Inf')
+                    else:
+                        thisLogP = log(thisP)
+                    if (thisLogP + preLogP > maxSum):
+                        maxSum = thisLogP + preLogP
+                        maxEnt = i - 1
+                        maxWord = utf8line[maxEnt + 1:index + 1] 
+                        inDict = True
+
+#Connect the characters that are not in the dictionarys together
+            if (inDict is False):
+                maxEnt = -1
+                maxWord = utf8line[:index + 1]
+                maxSum = len(maxWord) * log(1./sumN)
+                for i in xrange(index - 1, -1, -1):
+                    if (entryList[i].inDict):
+                        maxEnt = i
+                        maxWord = utf8line[i + 1:index + 1]
+                        maxSum = entryList[i].logP + len(maxWord) * log(1./sumN)
+                        break
+
+            if (maxEnt >= 0):
+                entryList.append(Entry(maxWord, maxSum, entryList[maxEnt], inDict))
             else:
-                if heap == []:
-                    updateHeap(heap, endIndex+1, input_str, Pw, preEntry.log_prob, preEntry)
-                continue
-        else:
-            chart[endIndex] = curEntry
-
-        updateHeap(heap, endIndex+1, input_str, Pw, curEntry.log_prob, curEntry)
-
-    curEntry = chart[len(input_str) - 1]
-    ans = []
-
-    while curEntry is not None:
-        ans.append(curEntry.word)
-        curEntry = curEntry.back_p
-
-    sys.stdout = codecs.lookup('utf-8')[-1](sys.stdout)
-    print ' '.join(reversed(ans)),
-
-if __name__ == '__main__':
-    main()
-
-
+                entryList.append(Entry(maxWord, maxSum, None, inDict))
+        print Entry.rollback(entryList[len(utf8line) - 1])
+sys.stdout = old
