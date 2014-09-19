e1eafa43b518fa33edd9f8d1172cbaf005a3cc95
Louis Ye <lvyuy@sfu.ca>
2014-09-19 12:27:50 -0700

    Adding bigram match, score 84.529 on server
    1) Not yet using punctuator seperating
    2) Still using threshold value as 7 for unigram
    3) Still connecting the consecutive characters that are not in dictionary
    4) Bigram model: P(B | A) = P(A, B) / P(A)

--- hw1_segmentation/lyuyuy/main.py
+++ hw1_segmentation/lyuyuy/main.py
@@ -9,18 +9,18 @@ optparser.add_option("-i", "--inputfile", dest="input", default=os.path.join('da
 (opts, _) = optparser.parse_args()
 
 from Entry import Entry
-from Pdist1w import Pdist1w
-from Pdist2w import Pdist2w
+from Pdist import Pdist
 from math import log
 
 old = sys.stdout
 sys.stdout = codecs.lookup('utf-8')[-1](sys.stdout)
 
-P1w = Pdist1w(opts.counts1w, singleCharIgnore = 7)
-P2w = Pdist2w(opts.counts2w)
-Pm = Pdist1w(opts.countsmark)
-sumN = P1w.N + P2w.N + Pm.N
-P1w.N = P2w.N = Pm.N = sumN
+P1w = Pdist(opts.counts1w, singleCharIgnore = 7)
+P2w = Pdist(opts.counts2w)
+
+# Pm = Pdist(opts.countsmark)
+# sumN = P1w.N + P2w.N + Pm.N
+# P1w.N = P2w.N = Pm.N = sumN
 
 with open(opts.input) as f:
     for line in f:
@@ -28,89 +28,59 @@ with open(opts.input) as f:
         entryList = []
         for index in range(len(utf8line)):
             maxSum = float('-Inf')
-            maxEnt = 0
+            maxEnt = -1
             maxWord = None
             inDict = False
-#Do the bygram match
-            """
-            headEntry = index - P1w.maxlen
-            if (headEntry < 0):
-                headEntry = 0
-            for i in range(headEntry, index + 1):
-                if (i == 0):
-                    preLogP = 0
-                    isHead = True
-                else:
-                    preLogP = entryList[i - 1].logP
-                    isHead = False
-                thisP = P2w(utf8line[i:index+1], isHead)
-                if (thisP is None):
-                    thisLogP = float('-Inf')
-                else:
-                    thisLogP = log(thisP["p"])
-                if (thisLogP + preLogP > maxSum):
-                    maxSum = thisLogP + preLogP
-                    maxEnt = i - 1
-                    maxWord = thisP["word"]
-                    inDict = True
-"""
-#Do the unigram match
-            if (inDict is False):
+
+#Do the gram match
+            if inDict is False:
                 headEntry = index - max(P1w.maxlen, P2w.maxlen)
                 if (headEntry < 0):
                     headEntry = 0
                 for i in range(headEntry, index + 1):
-                    if (i == 0):
-                        preLogP = 0
-                    else:
-                        preLogP = entryList[i - 1].logP
-                    thisP = P1w(utf8line[i:index+1])
-                    if (thisP is None):
-                        thisLogP = float('-Inf')
-                    else:
-                        thisLogP = log(thisP)
-                    if (thisLogP + preLogP > maxSum):
-                        maxSum = thisLogP + preLogP
-                        maxEnt = i - 1
-                        maxWord = utf8line[maxEnt + 1:index + 1] 
-                        inDict = True
+                    wordA = '' if i == 0 else entryList[i - 1].word
+                    wordB = utf8line[i:index+1]
 
-#Do the markgram match
-            if (inDict is False):
-                headEntry = index - Pm.maxlen
-                if (headEntry < 0):
-                    headEntry = 0
-                for i in range(headEntry, index + 1):
-                    if (i == 0):
-                        preLogP = 0
+# ***AB: P(B | A) = P(A, B) / P(A)
+                    P_AB = P2w(wordA + ' ' + wordB) # P(A, B)
+                    P_A = P1w(wordA)                # P(B)
+                    if P_AB is not None:
+                        log_P_B_base_A = log(P_AB) - (log(P_A) if P_A is not None else 0)
                     else:
-                        preLogP = entryList[i - 1].logP
-                    thisP = Pm(utf8line[i:index+1])
-                    if (thisP is None):
-                        thisLogP = float('-Inf')
-                    else:
-                        thisLogP = log(thisP)
-                    if (thisLogP + preLogP > maxSum):
+                        p1_wordB = P1w(wordB)
+                        log_P_B_base_A = log(p1_wordB) if p1_wordB is not None else float('-Inf')
+
+                    thisLogP = log_P_B_base_A
+                    preLogP  = 0 if i == 0 else entryList[i - 1].logP
+
+                    if (thisLogP + preLogP >= maxSum) or (maxSum == float('-Inf')):
                         maxSum = thisLogP + preLogP
                         maxEnt = i - 1
-                        maxWord = utf8line[maxEnt + 1:index + 1] 
+                        maxWord = utf8line[maxEnt + 1:index + 1]
+                    if (P1w(wordB) is not None) or (P2w(wordA + ' ' + wordB) is not None):
                         inDict = True
 
 #Connect the characters that are not in the dictionarys together
-            if (inDict is False):
+            if inDict is False:
                 maxEnt = -1
                 maxWord = utf8line[:index + 1]
-                maxSum = len(maxWord) * log(1./sumN)
-                for i in xrange(index - 1, -1, -1):
-                    if (entryList[i].inDict):
+                maxSum = len(maxWord) * log(1./P1w.N)
+                for i in range(index - 1, -1, -1):
+                    if entryList[i].inDict:
                         maxEnt = i
                         maxWord = utf8line[i + 1:index + 1]
-                        maxSum = entryList[i].logP + len(maxWord) * log(1./sumN)
+                        maxSum = entryList[i].logP + len(maxWord) * log(1./P1w.N)
                         break
 
-            if (maxEnt >= 0):
+            if maxEnt >= 0:
                 entryList.append(Entry(maxWord, maxSum, entryList[maxEnt], inDict))
             else:
                 entryList.append(Entry(maxWord, maxSum, None, inDict))
+
         print Entry.rollback(entryList[len(utf8line) - 1])
+
+        # for item in entryList:
+        #     print item.__str__(),
+        # print
+
 sys.stdout = old
