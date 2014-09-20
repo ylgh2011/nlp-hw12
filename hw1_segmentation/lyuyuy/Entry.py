e9b20e923793ccb63dbb9c9948f8e56d6cd146d5
Louis Ye <lvyuy@sfu.ca>
2014-09-20 13:55:51 -0700

    Score 86.993 on server
    1) Deal with another decimal point character

--- hw1_segmentation/lyuyuy/Entry.py
+++ hw1_segmentation/lyuyuy/Entry.py
@@ -1,12 +1,13 @@
 #This class define the Entry class
 class Entry:
-    def __init__(self, word, logP, backPnt = None, inDict = False, isNum = False, isCharNum = False):
+    def __init__(self, word, logP, backPnt = None, inDict = False, isNum = False, isUnitNum = False, isChNum = False):
         self.word = word
         self.logP = logP
         self.backPnt = backPnt
         self.inDict = inDict
         self.isNum = isNum
-        self.isCharNum = isCharNum
+        self.isUnitNum = isUnitNum
+        self.isChNum = isChNum
     def __str__(self):
         return '(word: ' + self.word + ', logP: ' + str(self.logP) + ', inDict:' + str(self.inDict) + ', isNum:' + str(self.isNum) + ', isCharNum:' + str(self.isCharNum) + ')'
 
