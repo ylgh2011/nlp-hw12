f364fbba2c09d901d01ff192010c2cf5f9f7cd13
Louis Ye <lvyuy@sfu.ca>
2014-10-05 17:24:57 -0700

    Modify pushLocalToRemote.sh
    1) As long as one command failed, termintate the script to avoid unexpected operation

--- hw2_chuncker/lvyuy/pushLocalToRemote.sh
+++ hw2_chuncker/lvyuy/pushLocalToRemote.sh
@@ -1,21 +1,28 @@
 #!/bin/bash
 
-if [ "$#" -ne 2 ]; then
+if [ $# -ne 2 ]; then
 	echo "Usage: scriptname <local branch> <remote branch>"
 	exit 1
 fi
 
-localBranch="$1"
-remoteBranch="$2"
+localBranch=$1
+remoteBranch=$2
 
 git checkout $remoteBranch
+[ $? -ne 0 ] && exit 1
 git pull
+[ $? -ne 0 ] && exit 1
 git checkout $localBranch
+[ $? -ne 0 ] && exit 1
 
 git rebase $remoteBranch
+[ $? -ne 0 ] && exit 1
 git push origin $localBranch:$remoteBranch
+[ $? -ne 0 ] && exit 1
 
 git checkout $remoteBranch
+[ $? -ne 0 ] && exit 1
 git pull
+[ $? -ne 0 ] && exit 1
 git checkout $localBranch
 
