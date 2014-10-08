b866e0a63e44f187b7bd813d4358e529de3a3588
Hexiang Hu <hexiangh@sfu.ca>
2014-10-07 19:05:06 -0700

    Dry run for average perceptron algorithm
    1) First test set

--- hw2_chuncker/hexiangh/method1/default.py
+++ hw2_chuncker/hexiangh/method1/default.py
@@ -1,4 +1,5 @@
 """
+### Averaged perceptron algorithm
 
 You have to write the perc_train function that trains the feature weights using the perceptron algorithm for the CoNLL 2000 chunking task.
 
@@ -34,87 +35,257 @@ from collections import defaultdict
 
 def perc_train(train_data, tagset, numepochs):
     feat_vec = defaultdict(float)
+    avg_feat_vec = defaultdict(float)
+    tau_feat_vec = dict()
     # insert your code here
     if len(tagset) <= 0:
         raise ValueError("Empty tagset")
 
-    numepochs = int(5)
+    numepochs = int(1)
     default_tag = tagset[0]
+    m = len(train_data) # length of training data
     for t in range(numepochs):
-        tmp = 0
-        # Count sentence
         print 'Iteration#',t,' is processing now.'
-        for (labeled_list, feat_list) in train_data:
+        for j, (labeled_list, feat_list) in enumerate(train_data):
             labels = copy.deepcopy(labeled_list)
             # add in the start and end buffers for the context
             # for every sentence in the training set, iterate numepochs times
             output = perc.perc_test(feat_vec, labeled_list, feat_list, tagset, default_tag)
             # compare current output and true result
             # correct_flag = True
-            feat_index = 0
-            # check word by word if the predicted tag is equal to the true tag
-            for i, v in enumerate(output):
-                (feat_index, feats) = perc.feats_for_word(feat_index, feat_list)
-                # retrieve the feature for a word
-                if len(feats) == 0:
-                    print >>sys.stderr, " ".join(labels), " ".join(feat_list), "\n"
-                    raise ValueError("features do not align with input sentence")
-                
-                fields = labels[i].split()
-                label = fields[2]
-                if i > 0: 
-                    label_pre = labels[i-1].split()[2]
-                    for feat in feats:
-                        if feat[0] == 'B': # for bigram feature
-                            feat_out = feat + ":" + output[i-1]  # feat_out is the "B:<previous output>"
-                            feat_lab = feat + ":" + label_pre  # feat_lab is the "B:<previous label>"
+            if j != m or t != numepochs - 1:
+                feat_index = 0
+                # check word by word if the predicted tag is equal to the true tag
+                for i, v in enumerate(output):
+                    (feat_index, feats) = perc.feats_for_word(feat_index, feat_list)
+                    # retrieve the feature for a word
+                    if len(feats) == 0:
+                        print >>sys.stderr, " ".join(labels), " ".join(feat_list), "\n"
+                        raise ValueError("features do not align with input sentence")
+                    
+                    label = labels[i].split()[2]
+                    if i > 0: 
+                        label_pre = labels[i-1].split()[2]
+                        for feat in feats:
 
-                            if   output[i-1] != label_pre and output[i] != label:
-                                feat_vec[feat_out, output[i]]   -= 1.0
-                                feat_vec[feat_lab, output[i]]   -= 1.0
-                                feat_vec[feat_out, label]       += 1.0
-                                feat_vec[feat_lab, label]       += 1.0
+                            if feat[0] == 'B': # for bigram feature
+                                feat_out = feat + ":" + output[i-1]  # feat_out is the "B:<previous output>"
+                                feat_lab = feat + ":" + label_pre  # feat_lab is the "B:<previous label>"
 
-                            elif output[i-1] == label_pre and output[i] != label:
-                                feat_vec[feat_lab, output[i]]   -= 2.0
-                                feat_vec[feat_lab, label]       += 2.0
+                                if   output[i-1] != label_pre and output[i] != label:
+                                    if (feat, output[i]) in tau_feat_vec:
+                                        (js, ts) = tau_feat_vec[feat, output[i]]
+                                        avg_feat_vec[feat, output[i]] = avg_feat_vec[feat, output[i]] + feat[feat, output[i]] * (t*m + j - ts*m - js)
 
-                            elif output[i-1] != label_pre and output[i] == label:
-                                pass
+                                    if (feat, label) in tau_feat_vec:
+                                        (js, ts) = tau_feat_vec[feat, label]
+                                        avg_feat_vec[feat, label] = avg_feat_vec[feat, label] + feat[feat, label] * (t*m + j - ts*m - js)
 
-                            elif output[i-1] == label_pre and output[i] == label:
-                                pass
 
-                            # feat_vec[feat_out, output[i]] = feat_vec[feat_out, output[i]] - 1
-                            # feat_vec[feat_lab, label] = feat_vec[feat_lab, label] + 1
 
-                            # feat_vec[feat_out, label] = feat_vec[feat_out, label] + 1
-                            # feat_vec[feat_lab, output[i]] = feat_vec[feat_lab, output[i]] - 1
+                                    # update original feature vector
+                                    feat_vec[feat_out, output[i]]   -= 1.0
+                                    feat_vec[feat_lab, output[i]]   -= 1.0
+                                    feat_vec[feat_out, label]       += 1.0
+                                    feat_vec[feat_lab, label]       += 1.0
 
-                        else: # for U00 to U22 feature
-                            feat_vec[feat, output[i]] = feat_vec[feat, output[i]] - 1.0
-                            feat_vec[feat, label] = feat_vec[feat, label] + 1.0
-                else:  # for i==0 case, all the first word in each sentence
-                    label_pre = 'B_-1'  # previous label will be denoted by B_-1
-                    for feat in feats:
-                        if feat[0] == 'B':  # bigram feature case
-                            feat = feat + ":" + label_pre
-                        feat_vec[feat, output[i]] = feat_vec[feat, output[i]] - 1.0
-                        feat_vec[feat, label] = feat_vec[feat, label] + 1.0
+                                    # update avg feature vector
+                                    avg_feat_vec[feat_out, output[i]]   -= 1.0
+                                    avg_feat_vec[feat_lab, output[i]]   -= 1.0
+                                    avg_feat_vec[feat_out, label]       += 1.0
+                                    avg_feat_vec[feat_lab, label]       += 1.0
 
-    m = len(train_data)
-    for key in feat_vec.keys():
-        feat_vec[key] = feat_vec[key]/(m*numepochs)
+                                    tau_feat_vec[feat_out, output[i]] = (j, t)
+                                    tau_feat_vec[feat_lab, output[i]] = (j, t)
+                                    tau_feat_vec[feat_out, label]     = (j, t)
+                                    tau_feat_vec[feat_lab, label]     = (j, t)
+
+                                elif output[i-1] == label_pre and output[i] != label:
+                                    if (feat, output[i]) in tau_feat_vec:
+                                        (js, ts) = tau_feat_vec[feat, output[i]]
+                                        avg_feat_vec[feat, output[i]] = avg_feat_vec[feat, output[i]] + feat[feat, output[i]] * (t*m + j - ts*m - js)
+
+                                    if (feat, label) in tau_feat_vec:
+                                        (js, ts) = tau_feat_vec[feat, label]
+                                        avg_feat_vec[feat, label] = avg_feat_vec[feat, label] + feat[feat, label] * (t*m + j - ts*m - js)
+
+                                    feat_vec[feat_lab, output[i]]   -= 2.0
+                                    feat_vec[feat_lab, label]       += 2.0
+
+                                    avg_feat_vec[feat_lab, output[i]]   -= 2.0
+                                    avg_feat_vec[feat_lab, label]       += 2.0
+                                    
+                                    tau_feat_vec[feat_lab, output[i]] = (j, t)
+                                    tau_feat_vec[feat_lab, label]     = (j, t)
+
+                                elif output[i-1] != label_pre and output[i] == label:
+                                    pass
+
+                                elif output[i-1] == label_pre and output[i] == label:
+                                    pass
+
+                            else: # for U00 to U22 feature
+
+                                if (feat, output[i]) in tau_feat_vec:
+                                    (js, ts) = tau_feat_vec[feat, output[i]]
+                                    avg_feat_vec[feat, output[i]] = avg_feat_vec[feat, output[i]] + feat[feat, output[i]] * (t*m + j - ts*m - js)
+
+                                if (feat, label) in tau_feat_vec:
+                                    (js, ts) = tau_feat_vec[feat, label]
+                                    avg_feat_vec[feat, label] = avg_feat_vec[feat, label] + feat[feat, label] * (t*m + j - ts*m - js)
+
+                                feat_vec[feat, output[i]] -= 1.0
+                                feat_vec[feat, label] += 1.0
+
+                                avg_feat_vec[feat, output[i]] -= 1.0
+                                avg_feat_vec[feat, label] += 1.0
+
+                                # update vector
+                                tau_feat_vec[feat, output[i]] = (j, t)
+                                tau_feat_vec[feat, label]     = (j, t)
+
+
+                    else:  # for i==0 case, all the first word in each sentence
+                        label_pre = 'B_-1'  # previous label will be denoted by B_-1
+                        for feat in feats:
+
+
+                            if feat[0] == 'B':  # bigram feature case
+                                feat = feat + ":" + label_pre
+
+                            if (feat, output[i]) in tau_feat_vec:
+                                (js, ts) = tau_feat_vec[feat, output[i]]
+                                avg_feat_vec[feat, output[i]] = avg_feat_vec[feat, output[i]] + feat[feat, output[i]] * (t*m + j - ts*m - js)
+
+                            if (feat, label) in tau_feat_vec:
+                                (js, ts) = tau_feat_vec[feat, label]
+                                avg_feat_vec[feat, label] = avg_feat_vec[feat, label] + feat[feat, label] * (t*m + j - ts*m - js)
+
+                            feat_vec[feat, output[i]] -= 1.0
+                            feat_vec[feat, label] += 1.0
+
+                            avg_feat_vec[feat, output[i]] -= 1.0
+                            avg_feat_vec[feat, label] += 1.0
+
+                            tau_feat_vec[feat, output[i]] = (j, t)
+                            tau_feat_vec[feat, label] = (j, t)
+
+
+            else:
+                # last sentence of each iteration
+                feat_index = 0
+                # check word by word if the predicted tag is equal to the true tag
+                for i, v in enumerate(output):
+                    (feat_index, feats) = perc.feats_for_word(feat_index, feat_list)
+                    # retrieve the feature for a word
+                    if len(feats) == 0:
+                        print >>sys.stderr, " ".join(labels), " ".join(feat_list), "\n"
+                        raise ValueError("features do not align with input sentence")
+                    
+                    fields = labels[i].split()
+                    label = fields[2]
+                    if i > 0: 
+                        label_pre = labels[i-1].split()[2]
+                        for feat in feats:
+                            if feat[0] == 'B': # for bigram feature
+                                feat_out = feat + ":" + output[i-1]  # feat_out is the "B:<previous output>"
+                                feat_lab = feat + ":" + label_pre  # feat_lab is the "B:<previous label>"
+
+                                if   output[i-1] != label_pre and output[i] != label:
+                                    if (feat, output[i]) in tau_feat_vec:
+                                        (js, ts) = tau_feat_vec[feat, output[i]]
+                                        avg_feat_vec[feat, output[i]] = avg_feat_vec[feat, output[i]] + feat[feat, output[i]] * (numepochs*m + m - ts*m - js)
+
+                                    if (feat, label) in tau_feat_vec:
+                                        (js, ts) = tau_feat_vec[feat, label]
+                                        avg_feat_vec[feat, label] = avg_feat_vec[feat, label] + feat[feat, label] * (numepochs*m + m - ts*m - js)
+
+                                    # update original feature vector
+                                    feat_vec[feat_out, output[i]]   -= 1.0
+                                    feat_vec[feat_lab, output[i]]   -= 1.0
+                                    feat_vec[feat_out, label]       += 1.0
+                                    feat_vec[feat_lab, label]       += 1.0
+
+                                    # update avg feature vector
+                                    avg_feat_vec[feat_out, output[i]]   -= 1.0
+                                    avg_feat_vec[feat_lab, output[i]]   -= 1.0
+                                    avg_feat_vec[feat_out, label]       += 1.0
+                                    avg_feat_vec[feat_lab, label]       += 1.0
+
+                                elif output[i-1] == label_pre and output[i] != label:
+                                    if (feat, output[i]) in tau_feat_vec:
+                                        (js, ts) = tau_feat_vec[feat, output[i]]
+                                        avg_feat_vec[feat, output[i]] = avg_feat_vec[feat, output[i]] + feat[feat, output[i]] * (numepochs*m + m - ts*m - js)
+
+                                    if (feat, label) in tau_feat_vec:
+                                        (js, ts) = tau_feat_vec[feat, label]
+                                        avg_feat_vec[feat, label] = avg_feat_vec[feat, label] + feat[feat, label] * (numepochs*m + m - ts*m - js)
+
+                                    feat_vec[feat_lab, output[i]]   -= 2.0
+                                    feat_vec[feat_lab, label]       += 2.0
+
+                                    avg_feat_vec[feat_lab, output[i]]   -= 2.0
+                                    avg_feat_vec[feat_lab, label]       += 2.0
+                                    
+
+                                elif output[i-1] != label_pre and output[i] == label:
+                                    pass
+
+                                elif output[i-1] == label_pre and output[i] == label:
+                                    pass
+
+                            else: # for U00 to U22 feature
+                                if (feat, output[i]) in tau_feat_vec:
+                                    (js, ts) = tau_feat_vec[feat, output[i]]
+                                    avg_feat_vec[feat, output[i]] = avg_feat_vec[feat, output[i]] + feat[feat, output[i]] * (numepochs*m + m - ts*m - js)
+
+                                if (feat, label) in tau_feat_vec:
+                                    (js, ts) = tau_feat_vec[feat, label]
+                                    avg_feat_vec[feat, label] = avg_feat_vec[feat, label] + feat[feat, label] * (numepochs*m + m - ts*m - js)
+
+                                feat_vec[feat, output[i]] -= 1.0
+                                feat_vec[feat, label] += 1.0
+
+                                avg_feat_vec[feat, output[i]] -= 1.0
+                                avg_feat_vec[feat, label] += 1.0
+
+
+                    else:  # for i==0 case, all the first word in each sentence
+                        label_pre = 'B_-1'  # previous label will be denoted by B_-1
+                        for feat in feats:
+                            if feat[0] == 'B':  # bigram feature case
+                                feat = feat + ":" + label_pre
+                            
+                            if (feat, output[i]) in tau_feat_vec:
+                                (js, ts) = tau_feat_vec[feat, output[i]]
+                                avg_feat_vec[feat, output[i]] = avg_feat_vec[feat, output[i]] + feat[feat, output[i]] * (numepochs*m + m - ts*m - js)
+
+                            if (feat, label) in tau_feat_vec:
+                                (js, ts) = tau_feat_vec[feat, label]
+                                avg_feat_vec[feat, label] = avg_feat_vec[feat, label] + feat[feat, label] * (numepochs*m + m - ts*m - js)
+
+                            feat_vec[feat, output[i]] -= 1.0
+                            feat_vec[feat, label] += 1.0
+
+                            avg_feat_vec[feat, output[i]] -= 1.0
+                            avg_feat_vec[feat, label] += 1.0
+
+
+        # end of iteration
+
+    # averaging perceptron
+    for key in avg_feat_vec.keys():
+        avg_feat_vec[key] = avg_feat_vec[key]/float(numepochs*m)
     # please limit the number of iterations of training to n iterations
-    return feat_vec
+    return avg_feat_vec
 
 if __name__ == '__main__':
     optparser = optparse.OptionParser()
     optparser.add_option("-t", "--tagsetfile", dest="tagsetfile", default=os.path.join("data", "tagset.txt"), help="tagset that contains all the labels produced in the output, i.e. the y in \phi(x,y)")
-    optparser.add_option("-i", "--trainfile", dest="trainfile", default=os.path.join("data", "train.txt.gz"), help="input data, i.e. the x in \phi(x,y)")
-    optparser.add_option("-f", "--featfile", dest="featfile", default=os.path.join("data", "train.feats.gz"), help="precomputed features for the input data, i.e. the values of \phi(x,_) without y")
-    # optparser.add_option("-i", "--trainfile", dest="trainfile", default=os.path.join("data", "train.dev"), help="input data, i.e. the x in \phi(x,y)")
-    # optparser.add_option("-f", "--featfile", dest="featfile", default=os.path.join("data", "train.feats.dev"), help="precomputed features for the input data, i.e. the values of \phi(x,_) without y")
+    # optparser.add_option("-i", "--trainfile", dest="trainfile", default=os.path.join("data", "train.txt.gz"), help="input data, i.e. the x in \phi(x,y)")
+    # optparser.add_option("-f", "--featfile", dest="featfile", default=os.path.join("data", "train.feats.gz"), help="precomputed features for the input data, i.e. the values of \phi(x,_) without y")
+    optparser.add_option("-i", "--trainfile", dest="trainfile", default=os.path.join("data", "train.dev"), help="input data, i.e. the x in \phi(x,y)")
+    optparser.add_option("-f", "--featfile", dest="featfile", default=os.path.join("data", "train.feats.dev"), help="precomputed features for the input data, i.e. the values of \phi(x,_) without y")
 
     optparser.add_option("-e", "--numepochs", dest="numepochs", default=int(10), help="number of epochs of training; in each epoch we iterate over over all the training examples")
     optparser.add_option("-m", "--modelfile", dest="modelfile", default=os.path.join("data", "default.model"), help="weights for all features stored on disk")
