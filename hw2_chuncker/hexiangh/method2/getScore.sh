572b25352ac64f1853651fe3e382fcdc8fe88c05
Hexiang Hu <hexiangh@sfu.ca>
2014-10-09 16:51:20 -0700

    Update the documentation for all the methods
    1) update the documentation for different algorithms

--- hw2_chuncker/hexiangh/method2/getScore.sh
+++ hw2_chuncker/hexiangh/method2/getScore.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-python Voting_MDR.py -m model.ignore
+python spec_avg_perc.py -m model.ignore
 [ $? -ne 0 ] && exit 1
 python perc.py -m model.ignore > output.ignore
 [ $? -ne 0 ] && exit 1
